71b3823b3bbcf5fd6acef35beb8dd4ff58bf74d1
souravmandall <souravmandalart@gmail.com>
2026-09-02 10:00:15 +0530

    Update cache version to dual-lens-v6

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dual-lens-v1';
+const CACHE = 'dual-lens-v6';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
